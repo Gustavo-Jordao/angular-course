82ec4cd2189a1f7d6b5b9d77e180fd6bcbc00c0d
Gustavo Jordão <gjordao@Gustavos-MacBook-Pro.local>
2017-03-24 12:21:33 +0000

    Added my files

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: '<div><h3><main-container></main-container></h3></div>'
+})
+
+export class App{
+
+}
